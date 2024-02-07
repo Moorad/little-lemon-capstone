7ab27a0bb3b2c866276e497b948b464f9d6390cb
Moorad <moorad2003@gmail.com>
2024-02-07 11:57:39 +0300

    Add booking API retrieve endpoint

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
+from rest_framework import viewsets
+from .models import MenuItem, Booking
+from .serializers import MenuItemSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
 def index(request):
     return render(request, "index.html", {})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
